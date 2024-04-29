dad8d32605aa98d9fcc5ad7eb2fa0cf648e46d9c
Eric Sanz <160590921+ESP15@users.noreply.github.com>
2024-04-29 11:56:09 +0200

    Add files via upload

---
+++ cartajoker/main.js
@@ -0,0 +1,10 @@
+function changeImage(){
+    document.getElementById("carta1").style.display="none";
+    document.getElementById("cambiocarta").style.display="block";
+}
+
+function restoreImage (){
+    document.getElementById("carta1").style.display="block";
+    document.getElementById("cambiocarta").style.display="none";
+
+}
